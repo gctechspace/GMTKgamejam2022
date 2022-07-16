efd9adccc682b83a7913c7a3f2cad0c9bfa744fc
Matt Paine <matt@mattsoftware.com>
2022-07-16 19:42:28 +1000

    Added the bomb script

---
+++ Assets/Scripts/Bomb.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bomb : MonoBehaviour
+{
+    public int Damage = 200;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnCollisionEnter(Collision collision) {
+        Health c = (collision.gameObject.GetComponent("Health") as Health);
+        if (c != null) {
+            Debug.Log("Collided with something with the health script attached");
+            c.Level -= Damage;
+            Destroy(gameObject);
+        }
+
+    }    
+}
