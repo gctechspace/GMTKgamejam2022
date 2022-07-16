a029e756d453921b80ceb1e472ce4f91c63cc697
secretnerd <35687413+heathykeithy@users.noreply.github.com>
2022-07-17 06:00:23 +1000

    added torches to platform generator.

--- Assets/Scripts/PlatformGenerator.cs
+++ Assets/Scripts/PlatformGenerator.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class PlatformGenerator : MonoBehaviour
 {
-    public GameObject PlatformPrefab;
+    public GameObject[] item;
     public int NumberOfObjectsToSpawn = 38;
     private int nextAngle = -120;
     private float height = 0.8f;
@@ -12,21 +12,30 @@ public class PlatformGenerator : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
-        for (int i = 0; i < NumberOfObjectsToSpawn; i++)
+
+        /*  for (int i = 0; i < NumberOfObjectsToSpawn; i++)
+          {
+              Instantiate(PlatformPrefab, new Vector3 (0, height, 0), Quaternion.Euler(new Vector3(0, -nextAngle, 0)));
+              nextAngle += 45;
+              height += 1.2f;
+
+          }*/
+
+        generate(item[0], NumberOfObjectsToSpawn, nextAngle, height);  //platform
+        generate(item[1], 30, nextAngle, 8); //torch
+
+    }
+
+    
+    void generate(GameObject obj, int units, int angle, float h )
+	{
+        for (int i = 0; i < units; i++)
         {
-            Instantiate(PlatformPrefab, new Vector3 (0, height, 0), Quaternion.Euler(new Vector3(0, -nextAngle, 0)));
-            nextAngle += 45;
-            height += 1.2f;
+            Instantiate(obj, new Vector3(0, h, 0), Quaternion.Euler(new Vector3(0, -angle, 0)));
+            angle += 45;
+            h += 1.2f;
 
         }
     }
-
-    // Update is called once per frame
-    void Update()
-    {
-       
-    }
-
 
 }
