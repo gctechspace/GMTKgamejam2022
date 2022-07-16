bac35644703db34cd1142b3b6dbe2f83a5fd4c03
secretnerd <35687413+heathykeithy@users.noreply.github.com>
2022-07-16 10:04:48 +1000

    added player cube with unwrap

--- Assets/cubeMover.cs
+++ Assets/cubeMover.cs
@@ -5,35 +5,54 @@ using UnityEngine.SceneManagement;
 
 public class cubeMover : MonoBehaviour
 {
-    public float speed = 5f;  //can be seen and edited and overwriten in the Unity Inspector tab
-    public float rotationSpeed = 100f;  
-
-
+    public float speed = 500f;  //can be seen and edited and overwriten in the Unity Inspector tab
+    
+    public float jumpForce = 50f;
+    float forward;
+    float rotation;
     private Rigidbody RB;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        RB = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        float forward = Input.GetAxis("Vertical"); //this can be "W" key,the up arrow key or a joystick/controller input.
+        forward = Input.GetAxis("Vertical"); //this can be "W" key,the up arrow key or a joystick/controller input.
    
        
-        transform.position = transform.position + transform.forward * speed * forward *Time.deltaTime;  //deltaTime keeps actions timing consitant in realation to framerate 
      
-        float rotation = Input.GetAxis("Horizontal");
+        rotation = Input.GetAxis("Horizontal");
 
-        transform.Rotate(Vector3.up * rotation * rotationSpeed*  Time.deltaTime); //method that rotates around axis(up or Y in this case)
+
         
 
-        if(transform.position.y < -1) //fall off , reset level
+		if (Input.GetButtonDown("Jump"))
+		{
+            jump();
+		}
+
+        if (transform.position.y < -1) //fall off , reset level
         {
             //requires "using UnityEngine.SceneManagement;
-            SceneManager.LoadScene("SampleScene"); //call scene by scene name string
+            SceneManager.LoadScene("tower"); //call scene by scene name string
         }
+    }
+	private void FixedUpdate()
+	{
+        // transform.position = transform.position + transform.forward * speed * forward * Time.deltaTime;  //deltaTime keeps actions timing consitant in realation to framerate 
+        RB.AddForce(rotation * speed * Time.deltaTime, 0, speed * forward * Time.deltaTime);
+
+
+        //transform.Rotate(Vector3.up * rotation * rotationSpeed * Time.deltaTime); //method that rotates around axis(up or Y in this case)
+
+    }
+    void jump()
+	{
+        //RB.velocity = new Vector3(RB.velocity.x, jumpForce, RB.velocity.z);
+        RB.AddForce(0, jumpForce, 0);
     }
 }
